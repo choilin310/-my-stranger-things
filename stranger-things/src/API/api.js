4b87f280dd31d551b33c143474c4c88d7949f809
choilin310 <choiinhwan0@gmail.com>
2023-04-18 22:22:00 -0400

    token

--- stranger-things/src/API/api.js
+++ stranger-things/src/API/api.js
@@ -11,3 +11,25 @@ export async function fetchAllPost() {
     console.error(error);
   }
 }
+
+export async function registerUser(username, password) {
+  try {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
--- stranger-things/src/components/LogInForm.jsx
+++ stranger-things/src/components/LogInForm.jsx
@@ -1,9 +1,36 @@
-import { useState } from "react"
+import { useState } from "react";
+import { registerUser } from "../API/api";
 
-export default function LogInForm(){
-    return(
-        <>
-        hello
-        </>
-    )
+export default function LogInForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+        const result = await registerUser(username, password);
+        console.log("Result in Component: ", result);
+    } catch (error) {
+        console.log(error); 
+    }
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
