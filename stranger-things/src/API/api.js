327c79909f5596fa42b1f6708b33bd15bdcb4c01
choilin310 <choiinhwan0@gmail.com>
2023-04-18 21:42:12 -0400

    first commit

---
+++ stranger-things/src/API/api.js
@@ -0,0 +1,13 @@
+const COHORT_NAME = "2301-FTB-ET-WEB-AM";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+
+export async function fetchAllPost() {
+  try {
+    const response = await fetch(`${BASE_URL}/posts`);
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
---
+++ stranger-things/src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route, Link } from "react-router-dom";
+import LogInForm from "./components/LogInForm";
+import AllPosts from "./components/AllPosts";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="head">
+        <h2>Stranger Things</h2>
+        <h3 className="links">
+          <Link to="/">HOME</Link>
+          <Link to="/posts">POSTS</Link>
+          <Link to="/users">LOGIN</Link>
+        </h3>
+      </header>
+
+      <Routes>
+        <Route path="/posts" element={<AllPosts />} />
+        <Route path="/users" element={<LogInForm />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
