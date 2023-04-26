d1f833cb91fb212f8a8bdb7ce3209111818b789c
choilin310 <choiinhwan0@gmail.com>
2023-04-26 16:18:21 -0400

    got the messages to render on my profile and added css

--- stranger-things/src/components/Profile.jsx
+++ stranger-things/src/components/Profile.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
+import { fetchMe } from "../API/api";
 import { useAuth } from "../hooks/useAuth";
 
 function Profile() {
   const { token, user, setToken } = useAuth();
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    async function getMessages() {
+      const APIresponse = await fetchMe(token);
+      setMessages(APIresponse.data.messages);
+    }
+    if (token && user) {
+      getMessages();
+    }
+  }, [token, user]);
 
   return (
-    <div>
-      {console.log("user from profile")}
-      <h1>Welcome - {token && `Welcome, ${user.username}`}</h1>
-      <h3>Our Posts - insert our posts here,</h3>
-      <h3>Our Messages </h3>
+    <div className="container">
+      <h1 className="title">{token && `Welcome, ${user.username}`}</h1>
+      <h3 className="subtitle">My messages</h3>
+      <ul className="messages-list">
+        {messages.map((message, index) => (
+          <li key={index} className="message">
+            {message.content}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
 export default Profile;
